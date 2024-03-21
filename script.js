753f26b5580d4610d4f64ae2817afb71e9374955
liannylm <liannylima_maia@hotmail.com>
2024-03-21 09:03:59 -0300

    Commit 21/03

---
+++ script.js
@@ -0,0 +1,16 @@
+let var1 = 5;
+let var2 = 10;
+
+let resultado1 = var1 + var2;
+let resultado2 = var1 - var2;
+
+console.log(var1 * var2);
+console.log(typeof(var1 / var2));
+
+console.log(resultado1);
+console.log(typeof(resultado1))
+
+console.log(resultado2);
+console.log(typeof(resultado2))
+
+console.log(var1 + var2);
