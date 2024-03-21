e7a0aebf9f2b51df0d6c702ca736cee4f2002e8d
liannylm <liannylima_maia@hotmail.com>
2024-03-21 10:27:44 -0300

    Commit pt3 21/03

--- script.js
+++ script.js
@@ -1,41 +1,52 @@
-let var1 = 5;
-let var2 = 10;
+let meuArray = ["cachorro", "gato", "coelho", "calopsita", "cobra", "peixe"];
 
-let resultado1 = var1 + var2;
-let resultado2 = var1 - var2;
-
-console.log(var1 * var2);
-console.log(typeof(var1 / var2));
-
-console.log(resultado1);
-console.log(typeof(resultado1));
-
-console.log(resultado2);
-console.log(typeof(resultado2));
-
-let variavelArray = (3, 6, 1, "hello", 40);
-console.log(variavelArray[3]);
-
-let variavelObjeto = {
-    nome: "Anny",
-    idade: 19,
-    data: "21/03/2024",
-    endereço: {
-        rua: "8 Norte",
-        condominio: {
-            nomeCondo: "Residencial xxxxxx",
-            apto: 1008,
-            bloco: "A"
-        },
-        bairro: 'Aguas Claras'
-    }
+for(let i = 0; i < 5; i++){
+    console.log(meuArray[i]);
 }
 
-console.log(variavelObjeto);
-console.log(variavelObjeto.nome);
 
-if(variavelObjeto.idade >= 18) {
-    console.log("Autorizado!");
-} else {
-    console.log("Sem permissão");
-}
+
+
+
+
+// let var1 = 5;
+// let var2 = 10;
+
+// let resultado1 = var1 + var2;
+// let resultado2 = var1 - var2;
+
+// console.log(var1 * var2);
+// console.log(typeof(var1 / var2));
+
+// console.log(resultado1);
+// console.log(typeof(resultado1));
+
+// console.log(resultado2);
+// console.log(typeof(resultado2));
+
+// let variavelArray = (3, 6, 1, "hello", 40);
+// console.log(variavelArray[3]);
+
+// let variavelObjeto = {
+//     nome: "Anny",
+//     idade: 19,
+//     data: "21/03/2024",
+//     endereço: {
+//         rua: "8 Norte",
+//         condominio: {
+//             nomeCondo: "Residencial xxxxxx",
+//             apto: 1008,
+//             bloco: "A"
+//         },
+//         bairro: 'Aguas Claras'
+//     }
+// }
+
+// console.log(variavelObjeto);
+// console.log(variavelObjeto.nome);
+
+// if(variavelObjeto.idade >= 18) {
+//     console.log("Autorizado!");
+// } else {
+//     console.log("Sem permissão");
+// }
