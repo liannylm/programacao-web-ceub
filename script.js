8918d0f8e66b81a53a18a3f98ebb8a5658b8b5b4
Lianny <162578021+liannylm@users.noreply.github.com>
2024-04-04 12:26:39 +0000

    JS pt2

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function converter() {
-    let valorUsuario = document.getElementById("valor-usuario").value;
+    let valorUsuario = document.getElementById("valorUsuario").value;
 
     let moedaOrigem = document.getElementById("moeda1").value
     let moedaDestino = document.getElementById("moeda2").value
