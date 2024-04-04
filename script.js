86e7d972e8d8b615a78e53dcf265a708c94836aa
Lianny <162578021+liannylm@users.noreply.github.com>
2024-04-04 12:23:00 +0000

    Alteração JS

---
+++ script.js
@@ -0,0 +1,10 @@
+function converter() {
+    let valorUsuario = document.getElementById("valor-usuario").value;
+
+    let moedaOrigem = document.getElementById("moeda1").value
+    let moedaDestino = document.getElementById("moeda2").value
+
+    console.log(moedaOrigem);
+    console.log(moedaDestino);
+    console.log(valorUsuario);
+}
